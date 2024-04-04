128f9bdc333c7ab2a232fdeafe07a71b6b47e98f
MRTater <925955711@qq.com>
2024-04-04 17:06:10 +0800

    Add dataloader converter

---
+++ judgement/MVT-3DVG/referit3d/in_out/pt_datasets/converter.py
@@ -0,0 +1,10 @@
+class Converter:
+    def __init__(self, args):
+        self.args = args
+        pass
+
+    def modify_trainset(self):
+        pass
+
+    def modify_testset(self):
+        pass
